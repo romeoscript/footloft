d9bdccf162c653d795c6d6ef1ee6f01755c92764
romeoscript <romeobourne211@gmail.com>
2026-02-09 12:21:04 +0100

    added scroll to top functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,46 +3,18 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import SearchBar from "@/components/SearchBar";
-import ShopContextProvider from "@/context/ShopContext";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Forever",
-  description: "Forever - E-commerce Application",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ShopContextProvider>
-          <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-            <ToastContainer />
-            <Navbar />
-            <SearchBar />
-            {children}
-            <Footer />
-          </div>
-        </ShopContextProvider>
-      </body>
-    </html>
+import ScrollToTop from "@/components/ScrollToTop";
+// ...
+<div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+  <ToastContainer />
+  <Navbar />
+  <SearchBar />
+  <ScrollToTop />
+  {children}
+  <Footer />
+</div>
+        </ShopContextProvider >
+      </body >
+    </html >
   );
 }
